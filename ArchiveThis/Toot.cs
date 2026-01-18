52bb06a669320010931a84183aaf1643e8bd207b
Ole Albers <ole@nocarrier.de>
2026-01-18 10:34:04 +0100

    Only respond when really asked to archive

--- ArchiveThis/Toot.cs
+++ ArchiveThis/Toot.cs
@@ -30,9 +30,17 @@ public class Toot
 
     private async Task HandleMentions(IEnumerable<Notification> mentions)
     {
+        var currentUser = await _client.GetCurrentUser();
         foreach (var mention in mentions.DistinctBy(q => q.Status!.InReplyToId))
         {
             if (mention.Status == null) continue;
+            var alreadyAnswered = await HasAlreadyAnswered(mention.Status, currentUser.Id);
+            if (alreadyAnswered)
+            {
+                _logger.LogDebug("I already answered. Do nothing");
+                continue;
+            }
+            
             var url = await GetUrlFromToot(mention.Status);
             if (url == null)
             {
@@ -44,8 +52,7 @@ public class Toot
 
             var archivUrl = $"http://archive.is/newest/{url}";
             _logger.LogDebug("Received request to store url '{url}'", url);
-            var response =
-                $"@{mention.Account.AccountName} \n\n{archivUrl}";
+            var response = $"@{mention.Account.AccountName} \nHere is your archived Website:\n\n💾 {archivUrl}";
 
             await SendToot(response, mention.Status.Id, mention.Status.Visibility);
         }
@@ -62,6 +69,20 @@ public class Toot
             var mentions = newNotifications.Where(q => q is { Status: not null, Type: "mention" });
             await HandleMentions(mentions);
         }
+    }
+
+    private async Task<bool> HasAlreadyAnswered(Status toot, string myId)
+    {
+        _logger.LogDebug("Trying to find out if I already answered");
+        _logger.LogDebug("Toot: {toot}", toot);
+
+        if (toot.Account.Id == myId)
+        {
+            return true;
+        }
+        if (toot.InReplyToId == null) return false;
+        var parent = await _client.GetStatus(toot.InReplyToId);
+        return await HasAlreadyAnswered(parent, myId);
     }
 
     private async Task<string?> GetUrlFromToot(Status toot)
